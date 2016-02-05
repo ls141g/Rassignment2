ffc97c98d167d561e1e16e02ddbbd247f49b947a
ls141g <ls141g@att.com>
2016-02-05 11:24:59 -0600

    hw assignment 2 R

---
+++ cachematrix.R
@@ -0,0 +1,33 @@
+## Assignment #3 to find inverse of matrix and cache globally
+
+## Below function creates a special "matrix" object that can cache its inverse.
+makeCacheMatrix <- function(x = matrix()){
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(solve) m <<- solve
+    getinverse<- function() m
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
+}
+
+
+## Below function computes the inverse of the special "matrix" returned by 
+## makeCacheMatrix above. If the inverse has already been calculated (and the 
+## matrix has not changed), then the cacheSolve should retrieve the inverse 
+## from the cache.
+cacheSolve <- function(x, ...){
+    m <- x$getinverse()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinverse(m)
+    m
+}
